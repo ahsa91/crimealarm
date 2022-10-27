616a804cae08d82f5c925b3839e244d126525eec
ahsa91 <ahsa91@gmail.com>
2022-10-27 22:48:21 +0100

    input validation and back button on register activity

---
+++ app/src/main/java/org/wit/crimealarm/activities/BaseActivity.kt
@@ -0,0 +1,41 @@
+package org.wit.crimealarm.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import org.wit.crimealarm.R
+
+/**
+ * A base activity class is used to define the functions and members which we will use in all the activities.
+ * It inherits the AppCompatActivity class so in other activity class we will replace the AppCompatActivity with BaseActivity.
+ */
+// START
+open class BaseActivity : AppCompatActivity() {
+
+    /**
+     * A function to show the success and error messages in snack bar component.
+     */
+    fun showErrorSnackBar(message: String, errorMessage: Boolean) {
+        val snackBar =
+            Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
+        val snackBarView = snackBar.view
+
+        if (errorMessage) {
+            snackBarView.setBackgroundColor(
+                ContextCompat.getColor(
+                    this@BaseActivity,
+                    R.color.colorSnackBarError
+                )
+            )
+        }else{
+            snackBarView.setBackgroundColor(
+                ContextCompat.getColor(
+                    this@BaseActivity,
+                    R.color.colorSnackBarSuccess
+                )
+            )
+        }
+        snackBar.show()
+    }
+}
--- app/src/main/java/org/wit/crimealarm/activities/RegisterActivity.kt
+++ app/src/main/java/org/wit/crimealarm/activities/RegisterActivity.kt
@@ -3,14 +3,15 @@ package org.wit.crimealarm.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import org.wit.crimealarm.R
 import org.wit.crimealarm.databinding.ActivityRegisterBinding
 
-class RegisterActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityRegisterBinding
+class RegisterActivity : BaseActivity() {
+    private lateinit var binding: ActivityRegisterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding= ActivityRegisterBinding.inflate(layoutInflater)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
         super.onCreate(savedInstanceState)
 
@@ -22,6 +23,87 @@ class RegisterActivity : AppCompatActivity() {
             // Launch the register screen when the user clicks on the text.
             val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
             startActivity(intent)
+        }
+
+        //call action bar
+        setupActionBar()
+
+        binding.btnRegister.setOnClickListener {
+
+            validateRegisterDetails()
+        }
+
+    }
+
+    /**
+     * A function for actionBar Setup.
+     */
+    private fun setupActionBar() {
+
+        setSupportActionBar(binding.toolbarRegisterActivity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
+        }
+
+        binding.toolbarRegisterActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    /**
+     * A function to validate the entries of a new user.
+     */
+    private fun validateRegisterDetails(): Boolean {
+        return when {
+            TextUtils.isEmpty(binding.etFirstName.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_first_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(binding.etLastName.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_last_name), true)
+                false
+            }
+
+            TextUtils.isEmpty(binding.etEmail.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                false
+            }
+
+            TextUtils.isEmpty(binding.etEmail.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), true)
+                false
+            }
+
+            TextUtils.isEmpty(binding.etConfirmPassword.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_enter_confirm_password),
+                    true
+                )
+                false
+            }
+
+            binding.etPassword.text.toString()
+                .trim { it <= ' ' } != binding.etConfirmPassword.text.toString()
+                .trim { it <= ' ' } -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_password_and_confirm_password_mismatch),
+                    true
+                )
+                false
+            }
+            !binding.cbTermsAndCondition.isChecked -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_agree_terms_and_condition),
+                    true
+                )
+                false
+            }
+            else -> {
+                showErrorSnackBar("Your details are valid.", false)
+                true
+            }
         }
     }
 }
