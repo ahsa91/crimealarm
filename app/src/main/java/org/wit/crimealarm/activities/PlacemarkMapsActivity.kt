62b1171a652aa2be1bc2f58f1fb90a3ad28b9c92
ahsa91 <ahsa91@gmail.com>
2022-10-31 02:15:41 +0000

    list of placemarks fully displayed

--- app/src/main/java/org/wit/crimealarm/activities/PlacemarkMapsActivity.kt
+++ app/src/main/java/org/wit/crimealarm/activities/PlacemarkMapsActivity.kt
@@ -1,32 +1,59 @@
 package org.wit.crimealarm.activities
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.ui.AppBarConfiguration
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import org.wit.crimealarm.R
 import org.wit.crimealarm.databinding.ActivityPlacemarkMapsBinding
 import org.wit.crimealarm.databinding.ContentPlacemarkMapsBinding
+import org.wit.crimealarm.main.MainApp
 
 
-class PlacemarkMapsActivity : AppCompatActivity() {
+class PlacemarkMapsActivity : AppCompatActivity(), GoogleMap.OnMarkerClickListener {
 
     private lateinit var binding: ActivityPlacemarkMapsBinding
     private lateinit var contentBinding: ContentPlacemarkMapsBinding
     lateinit var map: GoogleMap
-
+    lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        app = application as MainApp
         binding = ActivityPlacemarkMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //binding.toolbar.title = title
         setSupportActionBar(binding.toolbar)
-
         contentBinding = ContentPlacemarkMapsBinding.bind(binding.root)
         contentBinding.mapView.onCreate(savedInstanceState)
-
+        contentBinding.mapView.getMapAsync {
+            map = it
+            configureMap()
+        }
     }
 
+    fun configureMap() {
+        map.setOnMarkerClickListener(this)
+        map.uiSettings.setZoomControlsEnabled(true)
+        app.placemarks.findAll().forEach {
+            val loc = LatLng(it.lat, it.lng)
+            val options = MarkerOptions().title(it.title).position(loc)
+            map.addMarker(options).tag = it.id
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.zoom))
+        }
+    }
+
+    override fun onMarkerClick(marker: Marker): Boolean {
+        val currentTitle: TextView = findViewById(R.id.currentTitle)
+        currentTitle.text = marker.title
+
+        return false
+    }
 
     override fun onDestroy() {
         super.onDestroy()
