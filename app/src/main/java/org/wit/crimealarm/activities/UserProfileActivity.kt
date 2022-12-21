9e4df1b21fc2a7b7be730d704757b091e0f0bc1f
ahsa91 <ahsa91@gmail.com>
2022-12-21 22:46:27 +0000

    user profile activity design

---
+++ app/src/main/java/org/wit/crimealarm/activities/UserProfileActivity.kt
@@ -0,0 +1,12 @@
+package org.wit.crimealarm.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.wit.crimealarm.R
+
+class UserProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+    }
+}
