d3529074d550781b318771b156735186f594d35a
ahsa91 <ahsa91@gmail.com>
2022-12-22 21:22:09 +0000

    settings screen layout and activitycreated

---
+++ app/src/main/java/org/wit/crimealarm/activities/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package org.wit.crimealarm.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.wit.crimealarm.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
